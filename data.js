4bddd1c069e04e860ea501d1b6d7e90bbc690d74
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-05-24 17:00:33 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        name: "Cherry St.",
+        clothing: 8261.68,
+        equipment: 4810.34,
+        accessories: 1536.57
+    }, {
+        name: "Strawberry Mall",
+        clothing: 7875.87,
+        equipment: 3126.58,
+        accessories: 2019.81
+    }, {
+        name: "Peach St.",
+        clothing: 4990.23,
+        equipment: 4923.48,
+        accessories: 1472.59
+    }, {
+        name: "Lime Av.",
+        clothing: 4658.42,
+        equipment: 2955.55,
+        accessories: 1390.55
+    }, {
+        name: "Apple Rd.",
+        clothing: 3952.00,
+        equipment: 1858.46,
+        accessories: 917.90
+    }
+]
